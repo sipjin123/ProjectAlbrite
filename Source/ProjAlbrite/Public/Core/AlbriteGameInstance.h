1caccb3aa446e926019438ddc89cb15247928481
burlin_gauna <burlin_gauna@yahoo.com>
2025-03-26 01:12:57 +0800

    created custom game instance class / created blueprint versions of game mode and game instance to have custom blueprint accessibility

---
+++ Source/ProjAlbrite/Public/Core/AlbriteGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "AlbriteGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class PROJALBRITE_API UAlbriteGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
