e5d9ee47efb1b47e170aee2c4b308572866b309e
burlin_gauna <burlin_gauna@yahoo.com>
2025-03-25 18:39:13 +0800

    Imported Initial Source files

---
+++ Source/ProjAlbrite/ProjAlbriteGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjAlbriteGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjAlbriteGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjAlbriteGameMode();
+};
+
+
+
