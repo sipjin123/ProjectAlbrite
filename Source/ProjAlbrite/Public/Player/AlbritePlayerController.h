aed38fc46620f05f7139093610b23729a7181ef6
burlin_gauna <burlin_gauna@yahoo.com>
2025-03-26 01:23:38 +0800

    Created player controller and player state for CPP and Blueprints / Assigned new references to Game Mode

---
+++ Source/ProjAlbrite/Public/Player/AlbritePlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AlbritePlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJALBRITE_API AAlbritePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
