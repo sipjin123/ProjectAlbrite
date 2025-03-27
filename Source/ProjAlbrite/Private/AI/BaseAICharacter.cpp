68deab7ba8a45a226c1d11db4d0fb7a8f6c46dca
burlin_gauna <burlin_gauna@yahoo.com>
2025-03-27 22:27:10 +0800

    Created new AI Base character class and Blueprints for enemy minions and healers

---
+++ Source/ProjAlbrite/Private/AI/BaseAICharacter.cpp
@@ -0,0 +1,71 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/BaseAICharacter.h"
+
+// Sets default values
+ABaseAICharacter::ABaseAICharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	AbilitySystemComponent = CreateDefaultSubobject<UAlbriteAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+	
+	AttributeSet = CreateDefaultSubobject<UAlbriteAttributeSet>(TEXT("Attributes"));
+	
+	// Create and attach the widget component
+	CombatWidgetComponent = CreateDefaultSubobject<UWidgetComponent>(TEXT("CombatWidget"));
+	CombatWidgetComponent->SetupAttachment(RootComponent); // Attach to the character
+	CombatWidgetComponent->SetWidgetSpace(EWidgetSpace::World); // Set to World Space
+	CombatWidgetComponent->SetDrawSize(FVector2D(100, 10)); // Set UI size
+	CombatWidgetComponent->SetRelativeLocation(FVector(0, 0, 100)); // Position above head
+}
+
+// Called when the game starts or when spawned
+void ABaseAICharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (IsValid(AbilitySystemComponent))
+	{
+		AttributeSet = AbilitySystemComponent->GetSet<UAlbriteAttributeSet>();
+		AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
+			UAlbriteAttributeSet::GetHealthAttribute()).AddUObject(this, &ABaseAICharacter::OnHealthUpdated);
+
+		AbilitySystemComponent->RegisterGameplayTagEvent(
+			FGameplayTag::RequestGameplayTag(FName("Status.Invulnerable")),
+			EGameplayTagEventType::NewOrRemoved)
+			.AddUObject(this, &ABaseAICharacter::OnInvulnerableTagChanged);
+		
+		AbilitySystemComponent->RegisterGameplayTagEvent(
+			FGameplayTag::RequestGameplayTag(FName("Status.Stun")),
+			EGameplayTagEventType::NewOrRemoved)
+			.AddUObject(this, &ABaseAICharacter::OnStunTagChanged);
+	}
+
+	CombatWidget = Cast<UCombatUnitWidget>(CombatWidgetComponent->GetWidget());
+}
+
+// Called every frame
+void ABaseAICharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABaseAICharacter::OnHealthUpdated(const FOnAttributeChangeData& OnAttributeChangeData) const
+{
+	OnHealthChange.Broadcast(OnAttributeChangeData.NewValue);
+}
+
+void ABaseAICharacter::OnInvulnerableTagChanged(FGameplayTag GameplayTag, int NewVal) const
+{
+	OnInvulnerableChange.Broadcast(NewVal == 0 ? false : true);
+}
+
+void ABaseAICharacter::OnStunTagChanged(FGameplayTag GameplayTag, int NewVal) const
+{
+	OnStunChange.Broadcast(NewVal == 0 ? false : true);
+}
---
+++ Source/ProjAlbrite/Public/AI/BaseAICharacter.h
@@ -0,0 +1,75 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "GameFramework/Character.h"
+#include "Interfaces/IAlbriteCharacter.h"
+#include "Interfaces/IDamageable.h"
+#include "ActorComponents/AlbriteAbilitySystemComponent.h"
+#include "ProjAlbrite/ProjAlbriteCharacter.h"
+#include "Stats/AlbriteAttributeSet.h"
+#include "BaseAICharacter.generated.h"
+
+//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FInvulnerableChange, bool, InvulnerableValue);
+//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FStunChange, bool, StunValue);
+//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAttributeChange, int, AttributeValue);
+UCLASS()
+class PROJALBRITE_API ABaseAICharacter : public ACharacter, public IAbilitySystemInterface, public IIDamageable, public IIAlbriteCharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ABaseAICharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	/** Gameplay Ability System */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="GAS", meta = (AllowPrivateAccess="true"))
+	class UAlbriteAbilitySystemComponent* AbilitySystemComponent;
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override { return AbilitySystemComponent; };
+	
+	/** Gameplay Attributes */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="GAS", meta = (AllowPrivateAccess="true"))
+	const class UAlbriteAttributeSet* AttributeSet;
+	
+	/** Retrieves The base Attribute for Health **/
+	virtual float OnGetCurrentHealth_Implementation() override { return AttributeSet ? AttributeSet->Health.GetBaseValue() : 0.1f; };
+	
+	/** Retrieves The base Attribute for Max Health **/
+	virtual float OnGetMaxHealth_Implementation() override { return AttributeSet ? AttributeSet->MaxHealth.GetBaseValue() : 0.1f; };
+
+	/** This callback can be used by the UI. **/
+	UPROPERTY(BlueprintAssignable, Category = "Attribute callbacks")
+	FAttributeChange OnHealthChange;
+	void OnHealthUpdated(const FOnAttributeChangeData& OnAttributeChangeData) const;
+
+	UPROPERTY(BlueprintAssignable, Category = "Stat callbacks")
+	FInvulnerableChange OnInvulnerableChange;
+	
+	UPROPERTY(BlueprintAssignable, Category = "Stat callbacks")
+	FStunChange OnStunChange;
+	
+	void OnInvulnerableTagChanged(FGameplayTag GameplayTag, int NewVal) const;
+	void OnStunTagChanged(FGameplayTag GameplayTag, int NewVal) const;
+
+	virtual bool IsStunned_Implementation() override { return AbilitySystemComponent ? AbilitySystemComponent->ComponentHasTag(FName("Status.Stun")) : false; };
+	virtual bool IsInvulnerable_Implementation () override { return AbilitySystemComponent ? AbilitySystemComponent->ComponentHasTag(FName("Status.Invulnerable")) : false; };
+	virtual bool IsUnitDead_Implementation() override { return AttributeSet ? AttributeSet->Health.GetBaseValue() <= 0 : false; };
+
+	
+	UPROPERTY(BlueprintReadWrite, Category = "UI")
+	UCombatUnitWidget* CombatWidget;
+	
+	/** Widget Component for displaying UI above the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "UI", meta = (AllowPrivateAccess = "true"))
+	UWidgetComponent* CombatWidgetComponent;
+};
