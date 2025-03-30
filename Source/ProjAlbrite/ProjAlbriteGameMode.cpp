89dc6fa42786ca4c725e649828076c249a645b96
burlin_gauna <burlin_gauna@yahoo.com>
2025-03-30 19:17:57 +0800

    Modified game mode to allow spawning upon connecting as client / added host restriction and allowed editor override for host spawning / removed default pawn reference and assigned custom hero reference class

--- Source/ProjAlbrite/ProjAlbriteGameMode.cpp
+++ Source/ProjAlbrite/ProjAlbriteGameMode.cpp
@@ -2,14 +2,117 @@
 
 #include "ProjAlbriteGameMode.h"
 #include "ProjAlbriteCharacter.h"
-#include "UObject/ConstructorHelpers.h"
+#include "GameFramework/PlayerStart.h"
+#include "Kismet/GameplayStatics.h"
 
 AProjAlbriteGameMode::AProjAlbriteGameMode()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
+	DefaultPawnClass = nullptr;
+	bStartPlayersAsSpectators = true;
+}
+
+void AProjAlbriteGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AProjAlbriteGameMode::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+}
+
+void AProjAlbriteGameMode::PostLogin(APlayerController* NewPlayerController)
+{
+	Super::PostLogin(NewPlayerController);
+
+	// Check if the game is running as a listen server
+	if (GetNetMode() == NM_ListenServer)
 	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
+		// Check if this joining player is the listen server host
+		if (NewPlayerController->IsLocalController() || bIsAutoSpawn) 
+		{
+			UE_LOG(LogTemp, Warning, TEXT("The joining player is the host!"));
+			NewPlayerController->SetShowMouseCursor(false);
+			NewPlayerController->SetInputMode(FInputModeGameAndUI());
+		}
+		else
+		{
+			AAlbritePlayerController* CustomController = Cast<AAlbritePlayerController>(NewPlayerController);
+			SpawnCharacter(CustomController);
+		}
 	}
+	UE_LOG(LogTemp, Warning, TEXT("Player joined, waiting for manual spawn."));
+}
+
+void AProjAlbriteGameMode::HostGame()
+{
+	UWorld* World = GetWorld();
+	if (!World) return;
+
+	FString MapName = TEXT("AlbriteSandbox");
+
+	// Open map as a listen server
+	FString Command = FString::Printf(TEXT("%s?listen"), *MapName);
+	World->ServerTravel(Command);
+	
+	UE_LOG(LogTemp, Warning, TEXT("Host has Initialized!"));
+}
+
+void AProjAlbriteGameMode::RequestSpawn(AAlbritePlayerController* PlayerController)
+{
+	if (HasAuthority()) // Ensure we're on the server
+	{
+		SpawnCharacter(PlayerController);
+	}
+	else
+	{
+		ServerRequestSpawn(PlayerController);
+	}
+}
+
+void AProjAlbriteGameMode::ServerRequestSpawn_Implementation(AAlbritePlayerController* PlayerController)
+{
+	SpawnCharacter(PlayerController);
+}
+
+void AProjAlbriteGameMode::SpawnCharacter(AAlbritePlayerController* NewPlayerController)
+{
+	// Spawn the custom character
+	FVector SpawnLocation;
+	FRotator SpawnRotation;
+	AActor* PlayerSpawnPt = UGameplayStatics::GetActorOfClass(GetWorld(), APlayerStart::StaticClass());
+	if (PlayerSpawnPt)
+	{
+		SpawnLocation = PlayerSpawnPt->GetActorLocation();
+		SpawnRotation = PlayerSpawnPt->GetActorRotation();
+	}
+
+	// Spawn Player Character
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	ACharacter* SpawnedCharacter = GetWorld()->SpawnActor<AProjAlbriteCharacter>(LobbyCharacterClass, SpawnLocation, SpawnRotation, SpawnParams);
+	if (SpawnedCharacter)
+	{
+		NewPlayerController->Possess(SpawnedCharacter);
+		
+		AllControllers.Add(Cast<AAlbritePlayerController>(NewPlayerController));
+		UE_LOG(LogTemp, Warning, TEXT("Player Joined! Total Players: %d"), AllControllers.Num());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player Failed to Join Lobby!"));
+	}
+}
+
+void AProjAlbriteGameMode::Logout(AController* Exiting)
+{
+	Super::Logout(Exiting);
+
+	AAlbritePlayerController* PS = Cast<AAlbritePlayerController>(Exiting);
+	if (!PS)
+	{
+		return;
+	}
+
+	AllControllers.Remove(PS);
 }
--- Source/ProjAlbrite/ProjAlbriteGameMode.h
+++ Source/ProjAlbrite/ProjAlbriteGameMode.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "Player/AlbritePlayerController.h"
 #include "ProjAlbriteGameMode.generated.h"
 
 UCLASS(minimalapi)
@@ -13,6 +14,50 @@ class AProjAlbriteGameMode : public AGameModeBase
 
 public:
 	AProjAlbriteGameMode();
+
+	UPROPERTY(BlueprintReadWrite)
+	TArray<AAlbritePlayerController*> AllControllers;
+protected:
+
+	virtual void BeginPlay() override;
+
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+	virtual void PostLogin(APlayerController* NewPlayerController) override;
+
+	virtual void Logout(AController* Exiting) override;
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	bool IsThisDedicatedServer() {
+		return GetWorld()->GetNetMode() == NM_DedicatedServer;
+	}
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	bool IsThisListenServer() {
+		return GetWorld()->GetNetMode() == NM_ListenServer;
+	}
+	
+public:
+	
+	/** Character class that will be spawned for players */
+	UPROPERTY(EditDefaultsOnly, Category = "Lobby")
+	TSubclassOf<ACharacter> LobbyCharacterClass;
+
+	/** Simulate Host */
+	void HostGame();
+	
+	/** Handle client spawn request */
+	void RequestSpawn(AAlbritePlayerController* PlayerController);
+	void SpawnCharacter(AAlbritePlayerController* PlayerController);
+
+	/** Handle server spawn logic */
+	UFUNCTION(Server, Reliable, BlueprintCallable)
+	void ServerRequestSpawn(AAlbritePlayerController* PlayerController);
+	void ServerRequestSpawn_Implementation(AAlbritePlayerController* PlayerController);
+	bool ServerRequestSpawn_Validate();
+
+	/** Makes auto spawning characters for local host (Debugging) */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	bool bIsAutoSpawn = false;
 };
 
 
