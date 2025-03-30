13a17cb1196e3d0fb00584ab2dab2708f33630ff
burlin_gauna <burlin_gauna@yahoo.com>
2025-03-30 19:52:14 +0800

    Updated project server targets

---
+++ Source/ProjAlbriteServer.Target.cs
@@ -0,0 +1,18 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class ProjAlbriteServerTarget : TargetRules
+{
+	public ProjAlbriteServerTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Server;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+		ExtraModuleNames.Add("ProjAlbrite");
+		
+		// Disable client-related features
+		bUseLoggingInShipping = true;
+	}
+}
